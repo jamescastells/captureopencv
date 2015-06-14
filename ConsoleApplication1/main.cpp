b92e3089fe3492d30dd125498f51c94ceba47fc9
James Castells <james_castells@hotmail.com>
2015-06-14 15:58:50 -0500

    Actualizado

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,12 +1,12 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <thread>         // std::thread
 
 using namespace std;
 using namespace cv;
 
-int main(){
-
-	VideoCapture vcap(0);
+int capture(int device){
+	VideoCapture vcap(device);
 	if (!vcap.isOpened()){
 		cout << "Error opening video stream or file" << endl;
 		return -1;
@@ -14,16 +14,25 @@ int main(){
 
 	int frame_width = vcap.get(CV_CAP_PROP_FRAME_WIDTH);
 	int frame_height = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
-	VideoWriter video("out.avi", CV_FOURCC('M', 'J', 'P', 'G'), 25, Size(frame_width, frame_height), true);
+	
+	std::string s = std::to_string(device) + ".avi";
+
+	char const *pchar = s.c_str();  //use char const* as target type
+
+	VideoWriter video(pchar, CV_FOURCC('M', 'J', 'P', 'G'), 25, Size(frame_width, frame_height), true);
 
 	for (;;){
 
 		Mat frame;
 		vcap >> frame;
 		video.write(frame);
-		imshow("Frame", frame);
+		imshow(pchar, frame);
 		char c = (char)waitKey(33);
 		if (c == 27) break;
 	}
 	return 0;
+}
+
+int main(){
+	capture(0);
 }
